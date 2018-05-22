9ec046f322ef9dc3cfb9438addad795cbd975c28
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-22 20:17:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,9 @@ class Transfer
       if sender.balance > amount && @status = "pending"
         sender.balance -= amount
         receiver.balance += amount
+        @status = "complete"
+      else
+        @status = "rejected"
 
   end
 
