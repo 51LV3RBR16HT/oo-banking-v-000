3b31e98132a274b67c190dbe9ff6f9840fec98cc
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-22 20:18:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    binding.pry
+
       if sender.balance > amount && @status = "pending"
         sender.balance - amount
         receiver.balance + amount
