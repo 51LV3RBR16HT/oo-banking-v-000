f32b2e996799a9c277f5ff70c19fac707c3931ee
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-22 20:26:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,16 @@ class Transfer
   end
 
   def execute_transaction
-      if @sender.balance > @amount && @status == "pending"
-        @sender.balance -= @amount
-        @receiver.balance += @amount
-        @status = "complete"
+    if @sender.balance > @amount && @status == "pending"
+       @sender.balance -= @amount
+       @receiver.balance += @amount
+       @status = "complete"
       else
         @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
+
+  def
 
 end
