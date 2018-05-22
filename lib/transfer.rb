7e2c8ec2bcc8b09abdc378537dcadda63f87d46e
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-22 20:25:11 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,8 @@ class Transfer
         @status = "complete"
       else
         @status = "rejected"
-        return "Transaction rejected. Please check your account balance."
-      end
+      return "Transaction rejected. Please check your account balance."
     end
+  end
 
 end
