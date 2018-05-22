091027f881e2ae27d643ebc377ee420eb2c1d6d8
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-22 19:32:52 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
 
   attr_accessor :sender, :receiver, :status, :transfer
 
-  def initialize
+  def initialize(sender, receiver, status, transfer)
     @sender = sender
     @receiver = receiver
     @status = "pending"
